7251849ada78380fa9b83fc27d8ff0de27c708e7
sam-leishman <SAMUEL.LEISHMAN8727@STU.MTEC.EDU>
2020-12-18 07:46:10 -0700

    Add outline

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
+import { createStore } from 'redux';
 
 function App() {
   return (
-    <div>
-      <TestComponent />
+    <div className='container-fluid'>
+      <div className='main-page row no-gutters'>
+        <div className='channels col-3'>Channels</div>
+        <div className='chat-area col-9'>
+          <div className='messages-box'>Messages box</div>
+          <div className='message-input'>Input message</div>
+        </div>
+      </div>
     </div>
   );
-}
-
-class TestComponent extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Test</h1>
-      </div>
-    )
-  }
 }
 
 export default App;
