23ccff3acc1246b737162e18d7a64d1e0c352ee1
sam-leishman <SAMUEL.LEISHMAN8727@STU.MTEC.EDU>
2020-12-18 09:08:58 -0700

    Add ChatInput and render function

--- src/App.js
+++ src/App.js
@@ -2,17 +2,29 @@ import './App.css';
 import { Component } from 'react';
 import { createStore } from 'redux';
 import Header from './Header.js';
+import Chat from './Chat.js';
+import ChatInput from './ChatInput.js'
 
 function reducer(state, action) {
   if (action.type === 'ADD_MESSAGE') {
     return {
       messages: state.messages.concat(action.message)
     }
+  } else if (action.type === 'DELETE_MESSAGE') {
+    return {
+      messages: [
+        ...state.messages.slice(0, action.index),
+        ...state.messages.slice(action.index + 1, state.messages.length),
+      ],
+    }
+  } else {
+    return state;
   }
 }
 
 const initialState = { messages: [] };
-const store = createStore(reducer, initialState);
+
+export const store = createStore(reducer, initialState);
 
 function App() {
   return (
@@ -25,8 +37,9 @@ function App() {
         <div className='chat-area col-9'>
           <div className='messages-box'>Messages box</div>
           {/* <Chat /> */}
-          <div className='message-input'>Input message</div>
-          {/* <ChatInput /> */}
+          <div className='message-input'>
+            <ChatInput />
+          </div>
         </div>
       </div>
     </div>
---
+++ src/ChatInput.js
@@ -0,0 +1,46 @@
+import { Component } from "react";
+import { createStore } from "redux";
+import store from "./App.js";
+
+class ChatInput extends Component {
+    state = {
+        value: '',
+    }
+
+    onChange = (e) => {
+        this.setState({
+            value: e.target.value,
+        })
+    }
+
+    handleSubmit = () => {
+        store.dispatch({
+            type: 'ADD_MESSAGES',
+            message: this.state.value,
+        })
+        this.setState({
+            value: '',
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <input
+                    onChange={this.onChange}
+                    value={this.state.value}
+                    type='text'
+                />
+                <button
+                    onClick={this.handleSubmit}
+                    className='submit-button'
+                    type='submit'
+                >
+                    Send
+                </button>
+            </div>
+        )
+    }
+}
+
+export default ChatInput;
