7e070aa9955eec1cda1d3288c5162106a6d32ce2
UltraJag214 <hayden.gibbons8920@stu.mtec.edu>
2020-12-21 09:54:33 -0700

    trying to merge

--- src/App.js
+++ src/App.js
@@ -33,27 +33,7 @@ function reducer(state, action) {
   }
 }
 
-const initialState = {
-  currentChannelId: '1-fca2', // New state property
-  channels: [ // Two threads in state
-    {
-      id: '1-fca2', // hardcoded pseudo-UUID
-      title: 'Buzz Aldrin',
-      messages: [
-        { // This thread starts with a single message already
-          text: 'Test',
-          timestamp: fullDate,
-          id: uuidv4(),
-        },
-      ],
-    },
-    {
-      id: '2-be91',
-      title: 'Michael Collins',
-      messages: [],
-    },
-  ],
-};
+const initialState = { messages: [] };
 
 export const store = createStore(reducer, initialState);
 
@@ -64,10 +44,7 @@ class App extends Component {
   }
 
   render() {
-    const state = store.getState();
-    const currentChannelId = state.currentChannelId;
-    const channels = state.channels;
-    const currentChannel = channels.find((t) => t.id === currentChannelId);
+    const messages = store.getState().messages;
     return (
       <div className='container-fluid'>
         <div className="row no-gutters header">
@@ -85,12 +62,10 @@ class App extends Component {
             <Channels />
           </div>
           <div className='messages-box col-9'>
-            <Chat
-              channel={currentChannel}
-            />
+            <Chat messages={messages}/>
           </div>
           <div className='channel-input col-3'>
-            <ChannelInput />
+              <ChannelInput />
           </div>
           <div className='message-input col-9'>
             <ChatInput />
