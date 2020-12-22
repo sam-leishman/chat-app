5c60a42192350a6cb1c62f89f02193144397fd76
sam-leishman <SAMUEL.LEISHMAN8727@STU.MTEC.EDU>
2020-12-22 09:50:37 -0700

    Add some styling to the inputs

--- src/Compiler.js
+++ src/Compiler.js
@@ -38,7 +38,6 @@ class Compiler extends Component {
         </div>
         <div className='main-page row no-gutters'>
           <div className='channels col-3'>
-            <h5>Channels</h5>
             <Channels channelTabs={channelTabs} />
           </div>
           <div className='messages-box col-9'>
